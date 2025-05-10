a08b643ee89e2002b4b9e452b86ff317838ca141
Pedro Duarte <pdjtresr431@gmail.com>
2025-05-10 19:59:34 -0300

    proteção a números negativos

--- js/app.js
+++ js/app.js
@@ -5,18 +5,34 @@ let quant_inferior = parseInt(document.getElementById('qtd-inferior').innerHTML)
 function comprar() {
     let ingresso = document.getElementById('tipo-ingresso').value;
     let quantidade_ingresso = document.getElementById('qtd').value;
-    
-    if(ingresso == 'pista'){
-      quant_pista = quant_pista - quantidade_ingresso;
-      console.log(quant_pista);
+    if(quantidade_ingresso < 0){
+      alert("Insira um número positivo");
+    }
+    else{
+        if(ingresso == 'pista'){
+      quant_pista = quant_pista - quantidade_ingresso; 
+      if (quant_pista <= 0){
+        alert("Esgotado!");
+        quant_pista = 0;
+      }
+      document.getElementById('qtd-pista').innerHTML = `${quant_pista}`;
     }
     else if(ingresso == 'superior'){
-      quant_superior = quant_superior - quantidade_ingresso;
-      console.log(quant_superior);
+      quant_superior = quant_superior - quantidade_ingresso;   
+      if (quant_superior <= 0){
+        alert("Esgotado!");
+        quant_superior = 0;
+      }
+      document.getElementById('qtd-superior').innerHTML = `${quant_superior}`;
     }
     else{
      quant_inferior = quant_inferior - quantidade_ingresso;
-     console.log(quant_inferior);
+     if (quant_inferior <= 0){
+        alert("Esgotado!");
+        quant_inferior = 0;
+      }
+      document.getElementById('qtd-inferior').innerHTML = `${quant_inferior}`;
+    }    
     }
     
 }
